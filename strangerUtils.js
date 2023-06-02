0e92ac7e487d7acde4f6831c3fc9deb61438e960
Muhammed Murad <thomas@kali.google.com>
2023-06-01 20:13:16 -0400

    final

---
+++ strangerUtils.js
@@ -0,0 +1,24 @@
+/** @format */
+import * as wss from "wss.js";
+import * as webRTCHandler from "webRTCHandler.js";
+let strangerCallType;
+
+export const changeStrangerConnectionStatus = (status) => {
+  const data = { status };
+  wss.changeStrangerConnectionStatus(data);
+};
+
+export const getStrangerSocketIdAndConnect = (callType) => {
+  strangerCallType = callType;
+  wss.getStrangerSocketId();
+};
+
+export const connectWithStranger = (data) => {
+  console.log(data.randomStrangerSocketId);
+
+  if (data.randomStrangerSocketId) {
+    webRTCHandler.sendPreOffer(strangerCallType, data.randomStrangerSocketId);
+  } else {
+    console.log("no stranger user available");
+  }
+};
